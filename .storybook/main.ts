71954fb40ce468f7ca732700e0ec23616ac1227a
ykarmr <yuki.arimura0508@gmail.com>
2024-08-06 10:01:54 +0900

    tsconfig周りの修正とそれに伴うstorybookの修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,26 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite'
+const path = require('path')
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/addon-console",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-console',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-};
-export default config;
+  viteFinal: async (config) => {
+    config!.resolve!.alias = {
+      ...config!.resolve!.alias,
+      '@': path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
+}
+export default config
